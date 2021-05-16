9d6d1bb6a6882dfb7b779f5b3c6b2b12abe9435d
Deyvid Holz <deyvidholzt@gmail.com>
2021-05-16 02:01:39 -0300

    ♻️ call login service logic

--- src/login.controller.ts
+++ src/login.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { StatusCodes } from 'http-status-codes';
 import { LoginService } from './login.service';
 
 @Controller('login')
@@ -6,8 +7,11 @@ export class LoginController {
   constructor(private readonly loginService: LoginService) {}
 
   @Post()
+  @HttpCode(StatusCodes.OK)
   login(
     @Body('username') username: string,
     @Body('password') password: string,
-  ) {}
+  ) {
+    return this.loginService.login(username, password);
+  }
 }
