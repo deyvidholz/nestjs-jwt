cfb8f6c8115a72a41862e4298607637e26ee0357
Deyvid Holz <deyvidholzt@gmail.com>
2021-05-15 21:42:18 -0300

    ✨ add user entity

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ type: 'text' })
+  password: string;
+}
