dda4ae8a9bdd70e2f9aa09f9895f8e4c10fa0b36
Deyvid Holz <deyvidholzt@gmail.com>
2021-05-16 01:36:20 -0300

    🌱 add typeorm-seeding config

---
+++ typeorm-seeding.config.ts
@@ -0,0 +1,18 @@
+// TODO: add type
+const typeormForSeedConfig = {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: +process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database:
+    process.env.NODE_ENV !== 'test' ? process.env.POSTGRES_DB : 'auth_test_db',
+  autoLoadEntities: true,
+  entities: ['src/**/*.entity.{ts,js}'],
+  synchronize: true,
+  logging: true,
+  factories: ['src/**/*.factory.{ts,js}'],
+  seeds: ['src/**/*.seed.{.ts,.js}'],
+};
+
+export = typeormForSeedConfig;
