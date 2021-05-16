4b68d2a39e2161723d7a0413e4e2b11029230e27
Deyvid Holz <deyvidholzt@gmail.com>
2021-05-15 23:08:05 -0300

    ⚡️add index to user email column

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,10 +1,11 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'users' })
 export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Index()
   @Column({ unique: true })
   email: string;
 
