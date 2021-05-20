53d9281d39eeac71789ff0e22b9dbe5579418bee
Deyvid Holz <deyvidholzt@gmail.com>
2021-05-20 00:06:33 -0300

    ♻️ change login service import path

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { LoginController } from './controllers/login.controller';
-import { LoginService } from './factories/login.service';
+import { LoginService } from './services/login.service';
 
 @Module({
   imports: [
