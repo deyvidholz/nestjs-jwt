e9eefd14d9ccc2cb0886e94a261fb3085a6c3891
Deyvid Holz <deyvidholzt@gmail.com>
2021-05-20 00:16:01 -0300

    🚚 change jwt typings file name

--- src/services/login.service.ts
+++ src/services/login.service.ts
@@ -8,7 +8,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { LoginDTO } from '../dtos/login.dto';
 import { User } from '../entities/user.entity';
-import { JWT, JWTResult } from '../typings/jwt.typing';
+import { JWT, JWTResult } from '../typings/jwt.typings';
 
 @Injectable()
 export class LoginService {
