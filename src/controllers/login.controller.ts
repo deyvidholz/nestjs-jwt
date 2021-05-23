60e17a4608a94d471fd28dfb7537aea75ec00863
Deyvid Holz <deyvidholzt@gmail.com>
2021-05-23 19:29:03 -0300

    📝 set swagger decorators

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
 import { LoginDTO } from '../dtos/login.dto';
 import { LoginService } from '../services/login.service';
 import { JwtResult } from '../typings/jwt.typings';
@@ -7,6 +8,10 @@ import { JwtResult } from '../typings/jwt.typings';
 export class LoginController {
   constructor(private loginService: LoginService) {}
 
+  @ApiTags('Login')
+  @ApiBody({
+    type: LoginDTO,
+  })
   @Post()
   @HttpCode(HttpStatus.OK)
   login(@Body() loginDTO: LoginDTO): Promise<JwtResult> {
