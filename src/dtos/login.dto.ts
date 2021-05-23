fc30fe0ed4b86fc60aee8a52ec6277ceb804fb7c
Deyvid Holz <deyvidholzt@gmail.com>
2021-05-23 19:29:03 -0300

    📝 set swagger decorators

--- src/dtos/login.dto.ts
+++ src/dtos/login.dto.ts
@@ -1,4 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 export class LoginDTO {
+  @ApiProperty({
+    example: 'john@email.com',
+  })
   username: string;
+
+  @ApiProperty({
+    example: 'mypassword',
+  })
   password: string;
 }
